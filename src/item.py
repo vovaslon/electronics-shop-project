133d4f3c8e76fe57e0cf0f3253b2acece1147e88
kirillskyeng <117906913+kirillskyeng@users.noreply.github.com>
2023-08-23 16:20:04 +0300

    test done class done

--- src/item.py
+++ src/item.py
@@ -5,6 +5,7 @@ class Item:
     pay_rate = 1.0
     all = []
 
+
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
@@ -13,7 +14,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +25,15 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price*self.quantity
 
-    def apply_discount(self) -> None:
+
+
+    def apply_discount(self) -> float:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= self.pay_rate
+        return self.price
+
+
