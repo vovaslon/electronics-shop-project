b9a16feea7fd1d6296d7cf474b60fca1443d1f3f
vovaslon <vovaslon34@gmail.com>
2023-10-10 14:18:05 +0300

    repair tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -2,13 +2,11 @@
 from src.item import Item
 
 if __name__ == '__main__':
-    position1 = Item('Juice',45, 2)
+    def full_price():
+        position1 = Item('Juice',45, 2)
+        assert position1.calculate_total_price() == 90
 
-    """проверяем стоиомость"""
-    assert position1.calculate_total_price() == 90
-
-    """проверяем скидку"""
-    Item.pay_rate = 0.8
-    assert position1.apply_discount() == 36.0
-
-
+    def check_discount():
+        position1 = Item('Juice', 45, 2)
+        Item.pay_rate = 0.8
+        assert position1.apply_discount() == 36.0
