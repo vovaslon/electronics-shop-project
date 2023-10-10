bde914568357912d3a5f52de25f2f6131dc7653c
vovaslon <vovaslon34@gmail.com>
2023-10-10 14:23:22 +0300

    repair tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,12 +1,12 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
 
-if __name__ == '__main__':
-    def full_price():
-        position1 = Item('Juice',45, 2)
-        assert position1.calculate_total_price() == 90
 
-    def check_discount():
-        position1 = Item('Juice', 45, 2)
-        Item.pay_rate = 0.8
-        assert position1.apply_discount() == 36.0
+def full_price():
+    position1 = Item('Juice',45, 2)
+    assert position1.calculate_total_price() == 90
+
+def check_discount():
+    position1 = Item('Juice', 45, 2)
+    Item.pay_rate = 0.8
+    assert position1.apply_discount() == 36.0
